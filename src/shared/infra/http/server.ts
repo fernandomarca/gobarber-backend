7a5b9ffe4c3d4edab29c103faedb98bfa6789d6e
fernandomarca <fernandomarca@hotmail.com>
2020-12-29 01:13:19 -0300

    hiroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -44,6 +44,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   })
 });
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen((process.env.PORT || 3333), () => {
   console.log('server started on port 3333');
 });
